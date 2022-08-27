d3a161fa7cb7157ab1c12a80c371bd7f1ac99320
Big-Black-Cat <63571346+Big-Black-Cat@users.noreply.github.com>
2022-08-27 13:56:07 +0000

    finally

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ wm = WeChatMessage(client)
 # wea, temperature = get_weather()
 # data = {"date_current":{"value":get_currentdate()},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 wea, temperature ,low_temp, high_temp = get_weather()
-data = {"weather":{"value":wea},"low_temp":{"value":low_temp},"high_temp":{"value":high_temp},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"date_current":{"value":get_currentdate()},"weather":{"value":wea},"low_temp":{"value":low_temp},"high_temp":{"value":high_temp},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 
 res = wm.send_template(user_id, template_id, data)
 print(res)
